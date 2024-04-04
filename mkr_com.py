754f4c5ccca22239dacc37cd574bc29840159edb
Leonardo FM <leonardo.fajar.mardika-2020@ftmm.unair.ac.id>
2024-04-05 03:17:32 +0700

    create csv command

--- mkr_com.py
+++ mkr_com.py
@@ -2,6 +2,8 @@ import math
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import csv
+from datetime import datetime
 
 
 class Listener(Node):
@@ -15,6 +17,14 @@ class Listener(Node):
             qos_profile=10
         )
 
+        # Membuat nama file CSV berdasarkan waktu sekarang
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.csv_file_name = f"Sensor/sensor_data_{current_time}.csv"
+
+        # Membuka file CSV untuk ditulis
+        self.csv_file = open(self.csv_file_name, 'w', newline='')
+        self.csv_writer = csv.writer(self.csv_file)
+
     def callback(self, msg):
         sensor_data = msg.data
         self.get_logger().info(
@@ -22,18 +32,25 @@ class Listener(Node):
                 sensor_data
             )
         )
+        data_list = sensor_data.split(',')
+        self.csv_writer.writerow([x for x in data_list])
 
+    def close(self):
+        # Menutup file CSV
+        self.csv_file.close()
 
 def main(args=None):
     rclpy.init(args=args)
 
     listener = Listener()
 
-    rclpy.spin(listener)
-
-    listener.destroy_node()
-    rclpy.shutdown()
-
+    try:
+        rclpy.spin(listener)
+    except KeyboardInterrupt:
+        # Menutup node dan file CSV saat program berhenti
+        listener.close()
+        listener.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
