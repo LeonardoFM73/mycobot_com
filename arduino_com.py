0f414addb60b617e842bb86a922aa120dbc24cae
Leonardo FM <leonardo.fajar.mardika-2020@ftmm.unair.ac.id>
2024-04-13 18:44:46 +0700

    update arduino_com

--- arduino_com.py
+++ arduino_com.py
@@ -1,42 +1,54 @@
-import rospy
+import rclpy
+from rclpy.node import Node
 from std_msgs.msg import Int32
 
-def menu_sender():
-    rospy.init_node('gripper',anoymous=True)
+class Sender(Node):
+    def __init__(self):
+        super().__init__("Gripper")
 
-    pub = rospy.Publisher('menu', Int32, queue_size=10)
+    def start(self):
+        pub = self.create_publisher(
+            msg_type=Int32,
+            topic="menu",
+            qos_profile=10
+        )
+        rate = self.create_rate(30)  # 30hz
 
-    rate = rospy.Rate(1) 
+        while rclpy.ok():
+            print("Pilihan Menu:")
+            print("1. Diam")
+            print("2. Kirim nilai sensor ke ROS")
+            print("3. Kirim posisi servo ke ROS")
 
-    while not rospy.is_shutdown():
-        print("Pilihan Menu:")
-        print("1. Diam")
-        print("2. Kirim nilai sensor ke ROS")
-        print("3. Kirim posisi servo ke ROS")
+            # Minta pengguna untuk memilih menu (1, 2, atau 3)
+            menu_choice = input("Masukkan pilihan (1/2/3): ")
 
-        # Minta pengguna untuk memilih menu (1, 2, atau 3)
-        menu_choice = input("Masukkan pilihan (1/2/3): ")
+            try:
+                menu_choice = int(menu_choice)
+                if menu_choice < 1 or menu_choice > 3:
+                    Node.get_logger().warning("Pilihan tidak valid. Masukkan angka 1, 2, atau 3.", skip_first=True)
+                    continue
 
-        try:
-            menu_choice = int(menu_choice)
-            if menu_choice < 1 or menu_choice > 3:
-                rospy.logwarn("Pilihan tidak valid. Masukkan angka 1, 2, atau 3.")
-                continue
+                # Buat pesan untuk dikirim ke Arduino
+                msg = Int32()
+                msg.data = menu_choice
 
-            # Buat pesan untuk dikirim ke Arduino
-            msg = Int32()
-            msg.data = menu_choice
+                # Kirim pesan ke topik 'menu_choice'
+                pub.publish(msg)
+                rate.sleep()
 
-            # Kirim pesan ke topik 'menu_choice'
-            pub.publish(msg)
+            except Exception as e:
+                print(e)
 
-        except ValueError:
-            rospy.logerr("Masukan tidak valid. Masukkan angka 1, 2, atau 3.")
-
-        rate.sleep()
+def main(args=None):
+    rclpy.init(args=args)
+    
+    talker = Sender()
+    talker.start()
+    rclpy.spin(talker)
+    
+    talker.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
-    try:
-        menu_sender()
-    except rospy.ROSInterruptException:
-        pass
+    main()
